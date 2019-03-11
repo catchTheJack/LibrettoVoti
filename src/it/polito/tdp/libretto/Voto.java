fb799cf52785a4b568ba6f180da2ed549df51f34
giaco <giaco@LAPTOP-MI8VJRTQ.mshome.net>
2019-03-11 13:53:00 +0100

    Metodi di stampa

--- src/it/polito/tdp/libretto/Voto.java
+++ src/it/polito/tdp/libretto/Voto.java
@@ -1,5 +1,44 @@
 package it.polito.tdp.libretto;
 
-public class Voto {
+import java.time.LocalDate; ///libreria che permette di utilizzare piu agevolmente le date
 
+public class Voto {
+	private int voto;
+	private String corso;
+	private LocalDate data;
+	
+	public Voto(int voto, String corso, LocalDate data) {
+		super();
+		this.voto = voto;
+		this.corso = corso;
+		this.data = data;
+	}
+
+	public int getVoto() {
+		return voto;
+	}
+
+	public void setVoto(int voto) {
+		this.voto = voto;
+	}
+
+	public String getCorso() {
+		return corso;
+	}
+
+	public void setCorso(String corso) {
+		this.corso = corso;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	
+
+	
+	
 }
